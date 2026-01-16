8353e67829e23906038867b7fb16e67fb7ca897b
Paul <110413835+P-EXE@users.noreply.github.com>
2026-01-16 13:26:15 +0100

    Create README.md
    Update README.md
    
    Update README.md
    
    a
    
    a
    
    b
    
    a
    
    a
    
    a
    
    added mongodb
    
    Update README to clarify repository purpose
    
    Removed outdated text and clarified the purpose of the repository.
    
    changed gitlab
    
    a
    
    changed gitignore
    
    a
    
    bloop
    
    adadwed3

---
+++ README.md
@@ -0,0 +1,3 @@
+# Containerization
+
+This repo is mostly for the containers I run on my server. Feel free to copy the compose files or whatever 👍
--- gitlab/config/gitlab.rb
+++ gitlab/config/gitlab.rb
@@ -1,9 +1,18 @@
-# SSH Port
 gitlab_rails["gitlab_shell_ssh_port"] = 2424
 
-external_url "https://gitlab.pexe-server.lab.pexe.at"
+external_url "http://gitlab.blackbox.lab.p-exe.com"
 
+# Trafik handles TLS, so leave it off here
 letsencrypt["enable"] = false
-
 nginx["listen_port"] = 80
 nginx["listen_https"] = false
+
+# Performance optimizations for small deployments
+# These settings reduce resource usage but can lead to worse performance under high load
+postgresql['shared_buffers'] = '256MB'
+sidekiq['max_concurrency'] = 4
+sidekiq['concurrency'] = 1
+puma['worker_timeout'] = 120
+puma['worker_processes'] = 1
+# Disable prometheus for more performance at the cost of disabling monitoring
+prometheus_monitoring['enable'] = true
